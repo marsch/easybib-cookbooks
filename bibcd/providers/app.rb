b2d86ae1e82f712de5498928cb602f30242eddfa
fh <fh-github@fholzhauer.de>
2013-08-08 12:25:47 +0200

    trying tills new workaround idea https://github.com/till/easybib-cookbooks/commit/8a9db8f6a2507f4bc47a8d9d13e0ca507cd84200#commitcomment-3816012

--- bibcd/providers/app.rb
+++ bibcd/providers/app.rb
@@ -13,6 +13,13 @@ action :add do
       source "app.yml.erb"
     end
     
+    template "#{new_resource.path}/config/apps/#{new_resource.app_name}-testing-other-export.yml" do
+      cookbook "bibcd" #if we dont set this, the template cmd would search in the calling cookbook
+      mode   0644
+      variables :content => YAML::dump(Hash.new(new_resource.config))
+      source "app.yml.erb"
+    end
+    
     new_resource.updated_by_last_action(true)
   end
 end
